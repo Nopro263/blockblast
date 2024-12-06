54c7a1be4a9cbc540ba8608c21c3496d87b35484
Noah <82201000+Nopro263@users.noreply.github.com>
2024-12-06 21:32:46 +0100

    now with preview

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const ELEMENTS = [
         0b01110,
         0b00100,
         0b00000
+    ],
+    [
+        0b00000,
+        0b00100,
+        0b00100,
+        0b00100,
+        0b11100
     ]
 ]
 
@@ -65,7 +72,9 @@ const generateSource = (source, color) => {
     return element;
 }
 
-const insertElement = (source, color, targetElement, pos) => {
+const insertElement = (source, color, targetElement, pos, p="--color") => {
+    const actions = []
+
     for (let i = 0; i < 5; i++) {
         const row = source[i];
         for (let index = 0; index < 5; index++) {
@@ -76,28 +85,51 @@ const insertElement = (source, color, targetElement, pos) => {
                     return false;
                 }
             } else {
-                const targetSquare = getSquare(targetElement, i+pos[0], index+pos[1]);
-                if(targetSquare.style.getPropertyValue("--color")) {
-                    return false; // occupied
-                } else {
-                    if(v) {
-                        targetSquare.style.setProperty("--color", color)
+                if(v) {
+                    const targetSquare = getSquare(targetElement, i+pos[0], index+pos[1]);
+                    if(targetSquare.style.getPropertyValue("--color")) {
+                        return false; // occupied
+                    } else {
+                        actions.push(targetSquare);
                     }
                 }
             }
         }
     }
 
+    actions.forEach((v) => {
+        v.style.setProperty(p, color);
+    });
+
     return true;
+}
+
+const cleanElements = () => {
+    for (let a = 0; a < gameboard.children.length; a++) {
+        const element = gameboard.children[a];
+        for (let b = 0; b < element.children.length; b++) {
+            const e = element.children[b];
+            e.style.removeProperty("--default-color");
+        }
+    }
 }
 
 const sources = document.querySelector("#sources");
 const gameboard = document.querySelector(".gameboard");
 
 sources.appendChild(generateSource(ELEMENTS[0], "red"));
+sources.appendChild(generateSource(ELEMENTS[1], "green"));
 
 gameboard.addEventListener("dragover", (ev) => {
     ev.preventDefault();
+
+    cleanElements();
+    const {source, grabbingElement, color} = JSON.parse(ev.dataTransfer.getData("text"));
+
+    const gbPos = getRelLocation(ev.explicitOriginalTarget);
+    const pos = [gbPos[0]-grabbingElement[0], gbPos[1]-grabbingElement[1]]
+
+    insertElement(source, "yellow", gameboard, pos, "--default-color");
 });
 
 gameboard.addEventListener("drop", (ev) => {
@@ -106,8 +138,10 @@ gameboard.addEventListener("drop", (ev) => {
     const gbPos = getRelLocation(ev.explicitOriginalTarget);
     const pos = [gbPos[0]-grabbingElement[0], gbPos[1]-grabbingElement[1]]
 
-    console.log(insertElement(source, color, gameboard, pos));
+    if(insertElement(source, color, gameboard, pos)) {
+        const dragging = document.querySelector("#dragging");
+        dragging.parentElement.removeChild(dragging);
+    }
 
-    const dragging = document.querySelector("#dragging");
-    dragging.parentElement.removeChild(dragging);
+    
 })
